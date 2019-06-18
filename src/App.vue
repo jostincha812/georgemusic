c80d60819a7a892a9544ace5d70af550aa6bcd03
jostincha812 <jostin.cha812@gmail.com>
2019-06-18 08:33:29 -0500

    Expresiones en Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <h1>{{ mensaje }}</h1>
+      <p>{{ 1 + 1}}</p>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: "app",
+
+  data() {
+    return {
+      mensaje: "Hola Mundo"
+    };
+  }
+};
+</script>
+
+
 <style lang="scss">
 @import "./scss/main.scss";
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
