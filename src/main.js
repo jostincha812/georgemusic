a007739f58a3144c10e0a2ea80c0a2b00893bd16
jostincha812 <jostin.cha812@gmail.com>
2019-06-21 09:41:53 -0500

    Filtros en Vue js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import EventBus from "./plugins/event-bus";
 
 import router from "./router";
+import msToMm from "./filters/ms-to-mm";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
 Vue.use(EventBus);
+Vue.use(msToMm);
 
 new Vue({
   router,
