281b2ca84cc5496c2689e8c291207efce98eaae5
jostincha812 <jostin.cha812@gmail.com>
2019-06-21 12:19:32 -0500

    Mixins Vue Js

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,46 +1,74 @@
 <template>
   <div class="container">
     <div class="columns">
-      <div class="column is-5 is-offset-4">
-        <pm-loader v-show="isLoading"></pm-loader>
-        <pm-track v-bind:track="track" v-show="!isLoading"></pm-track>
+      <div class="column is-3 has-text-centered">
+        <figure class="media-left">
+          <p class="image">
+            <img v-bind:src="track.album.images[0].url">
+          </p>
+          <p>
+            <a class="button is-primary is-large">
+              <span class="icon" v-on:click="selectTrack"></span>
+            </a>
+          </p>
+        </figure>
+      </div>
+      <div class="column is-8">
+        <div class="panel">
+          <div class="panel-heading">
+            <h1 class="title">{{track.name}}</h1>
+          </div>
+          <div class="panel-block">
+            <article class="media">
+              <div class="media-content">
+                <div class="content">
+                  <ul v-for="(value, key) in track" v-bind:key="key">
+                    <li>
+                      <strong>
+                        {{key}}:&nbsp;
+                        <span>{{value}}</span>
+                      </strong>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <nav class="level">
+                <div class="level-left">
+                  <a class="level-item"></a>
+                </div>
+              </nav>
+            </article>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import PmTrack from "./Track.vue";
-import PmLoader from "./shared/Loader.vue";
-
+import trackMixin from "../mixins/track";
 import TrackId from "../services/trackId";
 
 export default {
-  components: {
-    PmTrack,
-    PmLoader
-  },
+  mixins: [trackMixin],
 
   data() {
     return {
-      track: {},
-      isLoading: false
+      track: {}
     };
   },
 
   created() {
-    this.isLoading = true;
     const id = this.$route.params.id;
     TrackId(id).then(response => {
       this.track = response;
-      this.isLoading = false;
     });
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.columns {
+.column {
   margin: 20px;
 }
 </style>
