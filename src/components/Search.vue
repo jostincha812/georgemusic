9d5a9d8fe23ccaf84f33613451f389d4a3a2c007
jostincha812 <jostin.cha812@gmail.com>
2019-06-21 09:22:02 -0500

    Modifiers en Vue.js

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -14,6 +14,7 @@
                 class="input is-medium is-rounded is-primary"
                 placeholder="Buscar Canciones"
                 v-model="searchQuery"
+                v-on:keyup.enter="search"
               >
             </div>
             <div class="control">
